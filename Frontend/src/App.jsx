6cd390df6a19c5ebddf93dac4d2ae1e39d893e46
eesa-ma <eesama30april04@gmail.com>
2025-09-15 03:04:58 +0530

    updated home page

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -4,8 +4,12 @@ import Videobanner from "./components/Videobanner"
 function App() {
   return (
     <>
-      <Header />
-      <Videobanner />
+      <div className="relative">
+        <Videobanner />
+        <div className="absolute z-10 top-0 right-0 left-0 p-5">
+          <Header />
+        </div>
+      </div>
     </>
   )
 }
