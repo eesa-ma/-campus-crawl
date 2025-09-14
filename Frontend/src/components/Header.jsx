07e466c10c92697575e54800a6b3285ab180bb79
eesa-ma <eesama30april04@gmail.com>
2025-09-14 14:30:20 +0530

    initial commit

---
+++ Frontend/src/components/Header.jsx
@@ -0,0 +1,12 @@
+
+function Header() {
+    return(
+        <>
+            <header>
+                <a href="#home" className="gistesy-font text-5xl md:text-6xl">CAMPUS CRAWL</a>
+            </header>
+        </>
+    );
+}
+
+export default Header;
