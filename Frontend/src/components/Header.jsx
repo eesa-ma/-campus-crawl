1af93196d7700b44cd44937327c9b10b4f3b3b53
eesa-ma <eesama30april04@gmail.com>
2025-09-19 01:01:05 +0530

    made works section responsive

--- Frontend/src/components/Header.jsx
+++ Frontend/src/components/Header.jsx
@@ -5,7 +5,7 @@ function Header() {
             <header className="w-full h-20 overflow-hidden border rounded-full bg-transparent">
                 <div className="w-full h-full flex items-center justify-around px-4 z-10">
                     <a href="#home" className="gistesy-font text-5xl ">Campus Crawl</a>
-                    <p className='Centurygothics-font text-xl'>Your smooth start to uni life</p>
+                    <p className='hidden md:flex Centurygothics-font text-xl'>Your smooth start to uni life</p>
                 </div>
             </header>
         </>
